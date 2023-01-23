728433958766fcdb9746c95e283f4c2516189b75
Ashish Gupta <ashish.g@cloudworkmates.com>
2023-01-23 18:07:28 +0530

    Modified

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export CI=true
-export CODEBUILD=true
-
 export CODEBUILD_GIT_BRANCH=`git symbolic-ref HEAD --short 2>/dev/null`
 if [ "$CODEBUILD_GIT_BRANCH" == "" ] ; then
   CODEBUILD_GIT_BRANCH=`git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }'`
